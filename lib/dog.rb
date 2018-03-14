b08f6235d6a66e192075a6dbbf5d47f8e0efbb93
Sean Oughton <seanoughton@gmail.com>
2018-03-14 19:50:53 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -46,7 +46,7 @@ class Dog
     #add the instance to the database
     #if self.id
     sql = <<-SQL
-      INSERT INTO dogs (name,breed)
+      INSERT INTO dogs (name,breed) VALUES 
     SQL
 
     DB[:conn].execute(sql)
