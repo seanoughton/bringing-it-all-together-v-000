8911b419478a268abc7494cb5c606d291eb9ab94
Sean Oughton <seanoughton@gmail.com>
2018-03-14 22:23:14 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -34,8 +34,7 @@ class Dog
       DB[:conn].execute(sql,self.name,self.breed)
       sql = "SELECT last_insert_rowid() FROM dogs"
       DB[:conn].execute(sql).flatten.first
-      self.id = DB[:conn].execute(sql).flatten.first
-      self
+      self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs").flatten.first
     end
 
   end
