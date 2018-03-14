47e1712796452c469ad1a2e4c58ea003ea45598a
Sean Oughton <seanoughton@gmail.com>
2018-03-14 19:08:54 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,7 @@ class Dog
   attr_accessor :name, :breed
   attr_reader :id
 
-  def initialize (attributes)
+  def initialize (name,breed)
     #attributes.each {|key,value| self.send(("#{key}="),value)}
     @name = attributes["name"]
     @breed = breed
